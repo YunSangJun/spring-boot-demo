ea6fc363a93e9d4460b0df7c964b935125113eec
sayoon <sayoo@ebay.com>
2019-12-12 16:02:02 +0900

    Add logback spring, print method

--- src/main/java/com/demo/test/TestController.java
+++ src/main/java/com/demo/test/TestController.java
@@ -2,12 +2,18 @@ package com.demo.test;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
 	private TestService testService;
+	private final Logger logger = LoggerFactory.getLogger(TestController.class);
 	
 	public TestController(TestService testService) {
 		this.testService = testService;
@@ -19,6 +25,18 @@ public class TestController {
 	@RequestMapping("/string")
 	public String getString() {
 		return testService.getString();
+	}
+
+	@RequestMapping(path = "/big/{count}", method = RequestMethod.GET)
+	public void printBigLog(@PathVariable("count") int count) {
+
+		String resultStr = "";
+
+		for(int i=0; i<count; i++){
+			resultStr = resultStr + "A";
+		}
+
+		logger.info(resultStr);
 	}
 	
 	@RequestMapping("/error")
